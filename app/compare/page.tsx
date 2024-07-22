4f04752f5acf8d82b8d8e29b23d6a01be7595fd6
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2024-07-23 02:52:11 +0530

    ♻️ Refactor code

--- app/compare/page.tsx
+++ app/compare/page.tsx
@@ -38,12 +38,12 @@ const compare = () => {
     e.preventDefault();
 
     try {
-      setProduct({
-        product: " ",
-      })
       setLoading(true);
       const response = await axios.post("/api/compare", product);
       setLoading(false);
+      setProduct({
+        product: " ",
+      })
       setResponseData(response.data);
       showToast("success", "Data retrieved successfuly");
     } catch (error: any) {
@@ -51,18 +51,18 @@ const compare = () => {
         product: " ",
       })
       setLoading(false);
-      showToast("error", error.response.statusText);
+      showToast("error", error.response.statusText + ", Try again");
     }
   };
 
   return (
-    <div>
+    <div className="md:p-0 p-2">
       <form
-        className="max-w-xl mx-auto bg-secondary-color shadow-md rounded px-8 pt-6 pb-8 mb-5"
+        className="max-w-xl mx-auto bg-secondary-color shadow-md rounded-xl px-8 pt-6 pb-8 mb-5"
         onSubmit={handleSubmit}
       >
-        <label className="block text-gray-700 text-md font-bold mb-2">
-          Product
+        <label className="block text-primary-color text-md font-bold mb-2">
+          Product / Model Name
         </label>
         <input
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -71,10 +71,11 @@ const compare = () => {
           name="product"
           value={product.product}
           onChange={onChange}
+          required
         />
         <div className="flex flex-col items-end">
           <button
-            className="bg-primary-color hover:bg-primary-dark-color text-white font-semibold text-sm py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4"
+            className="bg-secondary-dark-color hover:scale-110 transition-transform text-primary-color font-semibold text-sm py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4"
             type="submit"
           >
             Submit
@@ -84,7 +85,7 @@ const compare = () => {
       <h1 className="text-3xl font-extrabold tracking-tight text-center capitalize mt-10">
         Gemini's Opinion
       </h1>
-      <p className="mb-4 text-center max-w-xl m-auto mt-10 border-2 px-8 py-8 rounded-md">{responseData[0]}</p>
+      <p className="mb-4 text-center max-w-xl m-auto mt-10 border-2 border-secondary-color px-8 py-8 rounded-md">{responseData[0]}</p>
       <h1 className="text-3xl font-extrabold tracking-tight text-center capitalize mt-10">
         Products
       </h1>
