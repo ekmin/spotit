218a9bbc0f2d60953a8780482e842f16b71b3d44
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2024-07-27 01:55:16 +0530

    🐛 Fixed the deployment

---
+++ app/services/authOptions.ts
@@ -0,0 +1,43 @@
+import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { cert } from "firebase-admin/app";
+import { Adapter } from "next-auth/adapters";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+
+const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    }),
+  }) as Adapter,
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.uid = user.id;
+      }
+      return token;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+  },
+};
+
+export default authOptions;
